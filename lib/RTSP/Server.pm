b0d6c822b18056ca89fbd6a0cf22daa19097d580
Mischa S <revmischa@cpan.org>
2011-09-22 22:07:13 -0700

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:

--- lib/RTSP/Server.pm
+++ lib/RTSP/Server.pm
@@ -9,7 +9,7 @@ use RTSP::Server::Logger;
 use RTSP::Server::Source;
 use RTSP::Server::Client;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our $RTP_START_PORT = 20_000;
 
 ## configuration attributes
